2fcef69685ca57d623eaa55faf5dc08599f9326b
r4ihn <122985992+r4ihn@users.noreply.github.com>
2025-12-04 12:38:43 +0800

    More changes

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = FastAPI()
+class chatRequest(BaseModel): # Creating a class for what the user needs to provide
+    prompt: str
+
+class chatResponse(BaseModel): # Creating a class for an output, defines what the server needs to respond
+    response: str
+    
+app = FastAPI() # Creating a FastAPI instance
 
 @app.get("/") # This is an endpoint, an endpoint is a route that the user can access to get a response
 def health():
     return {"status": "ok"}
+
+@app.post("/chat")
+def chat(request: chatRequest): # This is a function that will be called when the user sends a request to the endpoint
+    return chatResponse(response="I will get stinkier later!")
