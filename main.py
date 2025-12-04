17f1479abe0ae1cfae51a7167aa4315b05ae46e3
r4ihn <122985992+r4ihn@users.noreply.github.com>
2025-12-04 12:22:57 +0800

    Added API Key

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") # This is an endpoint, an endpoint is a route that the user can access to get a response
 def health():
     return {"status": "ok"}
